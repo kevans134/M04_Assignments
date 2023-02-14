d27ff0aea74889884b28817ae6296b84407a70a5
kevans134 <91567352+kevans134@users.noreply.github.com>
2023-02-13 23:41:09 -0500

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,12 @@
+#KeShaundra Evans
+#2/13/2023
+#chapter 11
+#11.1 Create a file called zoo.py. In it, define a function called hours() that prints the string 'Open 9-5 daily'
+def hours():
+    print("open 9-5 daily")
+import zoo
+zoo.hours
+
+#11.2 In the interactive interpreter, import the zoo module as menagerie and call its hours() function
+import zoo as menagerie
+menagerie.hours()
